9a608da2800f0bde0abfa6808107654ef9f19e80
Philipp Mendogralo <abkxj59@gmail.com>
2022-10-07 01:41:33 +0500

    Тренеры, преимущества

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,12 +1,32 @@
-const trainersSlider = new Swiper('.trainers', {
-  loop: true,
-  // loopedSlides: 4,
+const trainers = document.querySelector('.trainers');
 
-  slidesPerView: 4,
-  // grid: {
-  //   rows: 2,
-  // },
-  spaceBetween: 40,
+trainers.classList.remove('trainers--nojs');
+
+const trainersSlider = new Swiper('#trainers-slider', {
+  // setWrapperSize: true,
+  loop: true,
+
+  breakpoints: {
+    320: {
+      // width: 200,
+      slidesPerView: 1,
+      spaceBetween: 47,
+    },
+    768: {
+      // width: 600,
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      // width: 1200,
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+
+  grid: {
+    rows: 1,
+  },
 
   navigation: {
     nextEl: '.swiper-button-next',
